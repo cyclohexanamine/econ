2728ab1eefd9ecd6f9593776aac51e3295c2575a
cyclohexanamine <cyclohexanamine@users.noreply.github.com>
2016-07-22 00:26:27 +0100

    Added debug functions

--- Grid.hs
+++ Grid.hs
@@ -190,3 +190,21 @@ makeNeighbour square =
 
 paramLK :: WorldState -> String -> Float
 paramLK ws n = head . map snd . filter ((==n).fst) . stateParams $ ws
+
+
+
+
+-- DEBUG
+
+iterN :: Float -> Int -> World -> World
+iterN t 0 w = w
+iterN t n w = updateWorld t . iterN t (n-1) $ w
+
+fromR = \(Right x) -> x
+
+lW :: IO (World)
+lW = fmap fromR . loadConfFile $ "in.txt"
+
+firstSq = (!!0) . worldSquares
+
+getGL = map fst . sqPrices . firstSq
